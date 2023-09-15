9ae8920663c3f71a0b1728dc6692b48c27470d28
anjitha <nanjitha89@gmail.com>
2023-09-15 08:45:06 +0300

    main

--- ele.py
+++ ele.py
@@ -1,15 +1,18 @@
-n=int(input("Enter a number "))
-if(n>0 and n<=100):
-    payAmount=n*0
+units=int(input("Enter a number "))
+if(units>0 and units<=100):
+    payAmount=units*0
     fixedcharge=0
-elif(n>100 and n<=200):
-    payAmount=(100*5)
-    fixedcharge=500.00
-elif(n>200 and n<=300):
-    payAmount=(200*10)
+elif(units>=100 and units<=200):
+    payAmount=(100*0)+(units-0)*5
+    fixedcharge=500
+
+elif(units>=200 and units<=300):
+    payAmount=(100*0)+(units-100)*10
     fixedcharge=2000
+
 else:
     payAmount=0
-    
-Total= payAmount
-print("\nElecticity bill pay=%.2f: " %Total)
+    fixedcharge=2500
+
+Total=payAmount+fixedcharge;
+print("\nElecticity bill=%.2f" %Total)
